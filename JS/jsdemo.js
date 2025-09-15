d3c17c2c154429ff9d67e1480d0447132e5904c8
Anas Khan <anas.23b0131150@abes.ac.in>
2025-09-15 13:01:24 +0530

    introduction to js

---
+++ JS/jsdemo.js
@@ -0,0 +1,17 @@
+
+var a=5;
+console.log(a);
+let b=1;c=12;
+const d=13;
+{
+    let e=14;
+    const f=40;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+}
+//   console.log(e);
+//     console.log(f);
---
+++ JS/jsfunction.js
@@ -0,0 +1,31 @@
+// function hello(user="guest"){   //default is guest
+//     console.log(`Hello ${user}`);
+// }
+// hello("anas");
+// hello();
+// function sum(a=0,b=0){
+//     console.log('Before return');
+//     return (a+b);
+// }
+// console.log(sum(23,34));
+// console.log(sum(1));
+// console.log(sum());
+
+// const hello=()=>console.log("Hello");  //anonymous function with arrow 
+// hello();
+const add=(a,b)=>(a+b);
+// console.log(add(4,6));
+//IIFE----function definition and calling at the same time
+// (function (){
+//     console.log("hello anas")
+// })();
+
+function hello(user,callback){
+    console.log(`Hello ${user}`);
+    callback();
+}
+hello('admin',()=>{
+   console.log(add(5,5))});
+hello('anas',function(){
+    console.log(add(9,6));
+})
